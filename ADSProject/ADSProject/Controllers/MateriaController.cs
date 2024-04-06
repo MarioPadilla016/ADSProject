66a03815a49ebdbe6420cc642040477ac3527288
MarioPadilla016 <padillamario078@gmail.com>
2024-04-06 09:58:10 -0600

    Se agregan validaciones para el modelo de Materia
    Se agregan validaciones para el modelo de Materia

--- ADSProject/ADSProject/Controllers/MateriaController.cs
+++ ADSProject/ADSProject/Controllers/MateriaController.cs
@@ -25,6 +25,10 @@ namespace ADSProject.Controllers
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
                 int contador = this.materia.AgregarMateria(materia);
                 if (contador > 0)
                 {
@@ -54,6 +58,10 @@ namespace ADSProject.Controllers
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
                 int contador = this.materia.ActualizarMateria(idMateria, materia);
                 if (contador > 0)
                 {
